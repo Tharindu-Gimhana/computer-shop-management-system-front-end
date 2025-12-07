ac29030e3b2b55c3ee6ad6b9ba244fcbc5d3e7e0
Tharindu-Gimhana <167947975+Tharindu-Gimhana@users.noreply.github.com>
2025-12-07 19:01:52 +0530

    create the view order button of admin side to see the datails of a order,  start the mobile responsive feature

--- vite-project/src/components/header.jsx
+++ vite-project/src/components/header.jsx
@@ -1,13 +1,22 @@
 import { Link, Route, Routes } from "react-router-dom";
 import { BiShoppingBag } from "react-icons/bi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
+
+
 
 
 
 export default function Header(){
+    const [sideBarOpen , setSideBarOpen] =  useState(false);
 return(
 <header className="w-full h-[100px] flex bg-[#051743] text-white flex items-center justify-between px-6 shadow-md fixed top-0 left-0 z-50 relative ">
-            <img src="/logo.png" className="h-full" alt="logo" />
-            <div className="w-full h-full flex text-xl text-primary justify-center items-center  gap-[30px]">
+            <GiHamburgerMenu className="lg:hidden text-primary text-2xl" onClick={()=> {
+                setSideBarOpen(true);
+            }
+            }/> 
+            <img src="./src/assets/react.svg" className="h-[70%]" alt="logo" />
+            <div className="w-full h-full text-xl hidden lg:flex text-primary justify-center items-center  gap-[30px]">
                 <Link to="/">Home</Link>
                 <Link to="/products">Products</Link>
                 <Link to="/about">About</Link>
@@ -17,6 +26,47 @@ return(
             <Link to="/cart" className="absolute right-4 top-1/2 -translate-y-1/2 text-primary text-2xl mr-10">
                 <BiShoppingBag />
             </Link>
+
+            {sideBarOpen && (
+				<div className="fixed lg:hidden w-[100vw] h-screen top-0 left-0 bg-black/50 z-20 transition-all duration-300">
+					<div className=" w-[250px] h-screen flex-col relative">
+						<div className="absolute w-full h-full bg-white left-[-250px] transform-flat translate-x-[250px] transition-transform duration-1000 flex flex-col">
+							<div className="w-full h-[100px] bg-accent flex justify-center items-center">
+								<img src="/logo.png" className="h-full" alt="logo" />
+								<GiHamburgerMenu
+									onClick={() => {
+										setSideBarOpen(false);
+									}}
+									className="text-white my-auto text-2xl ml-6 lg:hidden rotate-180"
+								/>
+							</div>
+                            <div className="w-full h-full flex flex-col text-xl text-secondary justify-start items-start gap-6 mt-10 pl-6">
+                                <a
+                                    className="hover:text-secondary transition"
+                                    href="/"
+                                    onClick={() => setSideBarOpen(false)}
+                                >
+                                    Home
+                                </a>
+                                <a
+                                    className="hover:text-secondary transition"
+                                    href="/products"
+                                    onClick={() => setSideBarOpen(false)}
+                                >
+                                    Products
+                                </a>
+                                <a
+                                    className="hover:text-secondary transition"
+                                    href="/about"
+                                    onClick={() => setSideBarOpen(false)}
+                                >
+                                    About
+                                </a>
+                            </div>
+						</div>
+					</div>
+				</div>
+			)}
 
 
             
